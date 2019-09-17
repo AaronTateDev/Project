9108234a9c41140cf1b877fb126a2fd85675b707
Aaron Tate <aaronjtate@gmail.com>
2019-09-16 19:36:47 -0500

    Nav Animations

--- js/main-cold.js
+++ js/main-cold.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 	$('#nav-icon').click(function(){
 		$(this).toggleClass('open');
+		if($('#pageNav').hasClass('hideMenuDropdown') == true) {
+			$('#pageNav').removeClass('hideMenuDropdown');
+		}else {
+			$('#pageNav').addClass('hideMenuDropdown');
+		}
 	});
 });
 
