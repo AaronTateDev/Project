a42e58dde3a42bab4b0e3801fb8da4692f64f710
Aaron Tate <aaronjtate@gmail.com>
2019-10-14 21:01:12 -0500

    Image popout testing

--- js/main-cold.js
+++ js/main-cold.js
@@ -1,13 +1,8 @@
 $(document).ready(function(){
 	$('#nav-icon').click(function(){
 		$(this).toggleClass('open');
-		$('#pageNav').toggleClass('hideMenuDropdown');
-		/*if($('#pageNav').hasClass('hideMenuDropdown') == false) {
-			$('#pageNav').removeClass('hideMenuDropdown');
-		}else {
-			$('#pageNav').addClass('hideMenuDropdown');
-		}*/
+		$('#pageNav').toggleClass('hideMenuDropdown');		
 	});
 });
 
-console.log("justice");
+
