b1174d67f05bfa6e418c93188162100b03dc1b99
Aaron Tate <aaronjtate@gmail.com>
2019-10-29 21:07:42 -0500

    @media finished just a few tweaks left

--- js/main-cold.js
+++ js/main-cold.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
 	$('#nav-icon').click(function(){
 		$(this).toggleClass('open');
-		$('#pageNav').toggleClass('hideMenuDropdown');		
+		$('#pageNav').toggleClass('hideMenuDropdown');
+		$('.enjoyTheCold').toggleClass('navDarken');
+		$('div#nav-icon').toggleClass('navDarkenBurger');		
+		$('.menuWordText').toggleClass('navDarken');
 	});
 });
+
+
+//ADD JS THAT CLOSES MENU ON CLICK
+//ADD CSS TO MAKE li text centered
 
 
 
